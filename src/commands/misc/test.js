dfceffc5c19659092a6d88590cecc74fbb86e5cb
mdrchu1s <mdrchuis@gmail.com>
2025-06-25 12:29:12 +0200

    just a space

--- src/commands/misc/test.js
+++ src/commands/misc/test.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Pong!!!',
     devOnly: true,
     testOnly: true,
-    //sheets: true,
+    // sheets: true,
     // barcOnly: true,
     // leadershipOnly: true,
     // options: Object[],
